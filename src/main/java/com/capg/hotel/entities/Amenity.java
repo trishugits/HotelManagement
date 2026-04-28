ec1a57f2e958e481aa6fffde571bfc0a6b4f470e
Ishank <deshwalishank@gmail.com>
2026-04-28 15:34:55 +0530

    Validation in Amenities

--- src/main/java/com/capg/hotel/entities/Amenity.java
+++ src/main/java/com/capg/hotel/entities/Amenity.java
@@ -1,6 +1,8 @@
 package com.capg.hotel.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Amenity")
@@ -10,11 +12,15 @@ public class Amenity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "amenity_id")
     private Integer amenityId;
-
-    @Column(name = "name")
+    
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "description")
+    
+    @NotBlank(message = "Description cannot be empty")
+    @Size(min = 5, max = 200, message = "Description must be between 5 and 200 characters")
+    @Column(name = "description", nullable = false)
     private String description;
 
 	public Integer getAmenityId() {
