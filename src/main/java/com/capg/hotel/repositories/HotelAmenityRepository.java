18f94f6b7ca94feb166e899eb77833f11790fb10
Riddhi <riddhisaraf8@gmail.com>
2026-04-28 12:17:15 +0530

    new endpoint

--- src/main/java/com/capg/hotel/repositories/HotelAmenityRepository.java
+++ src/main/java/com/capg/hotel/repositories/HotelAmenityRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.capg.hotel.dtos.HotelAmenityId;
 import com.capg.hotel.entities.HotelAmenity;
+import com.capg.hotel.projections.AmenityHotelsProjection;
 
 @RepositoryRestResource(
         path = "hotelamenities",
-        collectionResourceRel = "hotelamenities"
+        collectionResourceRel = "hotelamenities",
+        excerptProjection = AmenityHotelsProjection.class
 )
 interface HotelAmenityRepository extends JpaRepository<HotelAmenity,HotelAmenityId> {
     List<HotelAmenity> findByAmenityAmenityId(Integer amenityId);
+    List<HotelAmenity> findByHotelHotelId(Integer hotelId);
 }
--- src/main/java/com/capg/hotel/repositories/RoomAmenityRepository.java
+++ src/main/java/com/capg/hotel/repositories/RoomAmenityRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.capg.hotel.dtos.RoomAmenityId;
 import com.capg.hotel.entities.RoomAmenity;
+import com.capg.hotel.projections.AmenityRoomsProjection;
 
 @RepositoryRestResource(
         path = "roomamenities",
-        collectionResourceRel = "roomamenities"
+        collectionResourceRel = "roomamenities",
+        excerptProjection = AmenityRoomsProjection.class
 )
 interface RoomAmenityRepository extends JpaRepository<RoomAmenity,RoomAmenityId> {
     List<RoomAmenity> findByAmenityAmenityId(Integer amenityId);
